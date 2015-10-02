0973ab954f5c3023a438a0bcfb9424aaeafe924d
MARCOS ARMSTRONG <marcos.armstrong@mail.utoronto.ca>
2015-10-02 16:31:01 -0400

    Lab 1 Assignment

--- lab1.py
+++ lab1.py
@@ -7,24 +7,23 @@ __email__ = "ses@drsusansim.org"
 __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
 
-def vowel_or_consonant():
 
-    letter = raw_input('Type a letter here: ')
+letter = raw_input('Type a letter here: ')
 
-    if letter == "a":
-        print("vowel")
-    elif letter == "e":
-        print("vowel")
-    elif letter == "i":
-        print("vowel")
-    elif letter == "o":
-        print("vowel")
-    elif letter == "u":
-        print("vowel")
-    elif letter == "y":
-        print("sometimes a consonant")
-    else:
-        print("consonant")
+if letter == "a":
+    print("vowel")
+elif letter == "e":
+    print("vowel")
+elif letter == "i":
+    print("vowel")
+elif letter == "o":
+    print("vowel")
+elif letter == "u":
+    print("vowel")
+elif letter == "y":
+    print("sometimes a vowel")
+else:
+    print("consonant")
 
 # to run program, Run > Run... Prompt will appear.
 
